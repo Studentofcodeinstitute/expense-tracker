7dbea1bc009fb3974e8d019e8553ba9be1b9df53
Studentofcodeinstitute <>
2023-08-03 18:36:22 +0000

    Add reset fanction for input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,26 @@
 //let button = document.getElementById("clicked");
 let row = 1;
-function tableDetails() {
+resetForm();
+
+
+
+function tableDetails(event) {
+    event.preventDefault();
+
     let name = document.getElementById('name').value;
     let date = document.getElementById('date').value;
     let type = document.getElementById('type').value;
     let amount = document.getElementById('amount').value;
-    let html = `<button onclick="onClick(this)">Delete</button>`;
+    let html = `<button onclick="onClick(this);">Delete</button>`;
 
     if (!name || !date || !type || !amount) {
         alert(`Unknown game type`);
-        // return;
+        return;
     }
 
     let tableElement = document.getElementById('expense-table');
 
-    let rows = tableElement.insertRow(row);
+    let rows = tableElement.insertRow(-1);
 
     let cell1 = rows.insertCell(0);
     let cell2 = rows.insertCell(1);
@@ -30,13 +36,33 @@ function tableDetails() {
 
     row++;
 
+
+    localStorage.setItem("name", JSON.stringify(cell1));
+    localStorage.setItem("date", JSON.stringify(cell2));
+    localStorage.setItem("type", JSON.stringify(cell3));
+    localStorage.setItem("amount", JSON.stringify(cell4));
+    localStorage.setItem("html", JSON.stringify(cell5));
+
+}
+
+function resetForm() {
+    document.getElementById('name').value = "";
+    document.getElementById('date').value = "";
+    document.getElementById('type').value = "";
+    document.getElementById('amount').value = "";
 }
 
 function onClick(td) {
+
     if (confirm('Are You Sure')) {
         row = td.parentElement.parentElement;
-        document.getElementById("expense-table").deleteRow(row.rowIndex);
+        document.getElementById('expense-table').deleteRow(row.rowIndex);
         resetForm();
+
+
     }
+
+    return;
 }
+
 //button.addEventListener("click", tableDetails);
