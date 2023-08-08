b10c3de044c55a284740ed54508ac0e2b7feedce
Studentofcodeinstitute <>
2023-08-08 15:20:39 +0000

    Add localStorage to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,44 @@
 let row = 1;
 resetForm();
 
+const deleteHTML = `<button onclick="onClick(this);">Delete</button>`;
 
+
+function getCurrentLocalStorageValue() {
+    let currentData = localStorage.getItem("data") 
+    try {
+       currentData = JSON.parse(currentData) || [];
+   } catch {
+       currentData = [];
+   } 
+
+   return currentData;
+}
+
+function addDataToLocalStorage(newRow) {
+    const currentData = getCurrentLocalStorageValue();
+    const newData = [...currentData, newRow];
+    localStorage.setItem("data", JSON.stringify(newData)); 
+}
 
 function tableDetails(event) {
-    event.preventDefault();
+    //event.preventDefault();
 
     let name = document.getElementById('name').value;
     let date = document.getElementById('date').value;
     let type = document.getElementById('type').value;
     let amount = document.getElementById('amount').value;
-    let html = `<button onclick="onClick(this);">Delete</button>`;
+    
 
     if (!name || !date || !type || !amount) {
         alert(`Unknown game type`);
         return;
     }
+ 
+   let tableElement = document.getElementById('expense-table');
 
-    let tableElement = document.getElementById('expense-table');
-
-    let rows = tableElement.insertRow(-1);
-
+   let rows = tableElement.insertRow(-1);
+ 
     let cell1 = rows.insertCell(0);
     let cell2 = rows.insertCell(1);
     let cell3 = rows.insertCell(2);
@@ -32,19 +50,9 @@ function tableDetails(event) {
     cell2.textContent = date;
     cell3.textContent = type;
     cell4.textContent = amount;
-    cell5.innerHTML = html;
+    cell5.innerHTML = deleteHTML;
 
-    row++;
-
-/**
- 
- 
-    * localStorage.setItem("name", JSON.stringify(cell1));
-    * localStorage.setItem("date", JSON.stringify(cell2));
-    * localStorage.setItem("type", JSON.stringify(cell3));
-    * localStorage.setItem("amount", JSON.stringify(cell4));
-    * localStorage.setItem("html", JSON.stringify(cell5));
-*/
+    addDataToLocalStorage({name, date, type, amount});
 }
 
 function resetForm() {
@@ -55,16 +63,43 @@ function resetForm() {
 }
 
 function onClick(td) {
-
     if (confirm('Are You Sure')) {
         row = td.parentElement.parentElement;
         document.getElementById('expense-table').deleteRow(row.rowIndex);
         resetForm();
-
-
     }
 
     return;
 }
 
-//button.addEventListener("click", tableDetails);
+function tableReset(){
+    if (confirm('Are you sure')) {
+        
+        localStorage.setItem("data", JSON.stringify(""))
+        
+    };
+        
+    }
+
+
+
+function initialiseTable() {
+    const tableData = getCurrentLocalStorageValue();
+    const tableElement = document.getElementById('expense-table');
+    tableData.forEach((eachRow) => {
+        let rows = tableElement.insertRow(-1);
+        let cell1 = rows.insertCell(0);
+        let cell2 = rows.insertCell(1);
+        let cell3 = rows.insertCell(2);
+        let cell4 = rows.insertCell(3);
+        let cell5 = rows.insertCell(4);
+
+        cell1.textContent = eachRow.name;
+        cell2.textContent = eachRow.date;
+        cell3.textContent = eachRow.type;
+        cell4.textContent = eachRow.amount;
+        cell5.innerHTML = deleteHTML;
+    });
+}
+
+document.onload = initialiseTable();
