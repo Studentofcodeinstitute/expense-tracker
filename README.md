bd491f93ee42cfe235e0f64231f075d97bb90334
Studentofcodeinstitute <>
2023-08-09 13:03:22 +0000

    Add image to README.md file

--- README.md
+++ README.md
@@ -1,12 +1,96 @@
 # portfolio Project 2
 
+
+
 ## Viewport image.
+
+
 ### The webpage restrain a page, constructed by HTML, CSS and JavaScript.
 
 
-!![alt text](assets/images/Screenshot%20of%20mainpage.png)
+![alt text](assets/images/mainViewPort.png)
 
 
-## First section of  website
+## First section of  webpage
 
-It
+It's assembled by two part;
+
+
+1. it is a header, contain a title name that satisfies own goal "expense tracker"
+2. It is a input option, specified for ectual user;
+      1. Name.
+      2. Date of expense.
+      3. Type. there are two options; Cash or Credit-card.
+      4. Amount spended.
+   
+   
+![alt text](assets/images/first-section1.png)
+
+
+## Second section of  webpage
+
+It's assembled by three part;
+
+1. Contain "Add Expense" button to add records of expenses in the table.
+2. "Delete all" button to delete all rows.
+3. The table, contain;
+       1. Name.
+       2. Date.
+       3. Type.
+       4. Amount.
+       5. Delete button. It is for delete a row data, needs permission before action.
+   
+
+
+
+![alt text](assets/images/section2.png)
+
+
+![alt text](assets/images/second-section2.png)
+
+# Validator Testing
+
+1. HTML
+   No errors were returned when passing through the official W3C validator.
+
+2. CSS
+   No errors were found when passing through the official (Jigsaw) validator.
+
+3. Accessibility
+
+![alt text](assets/images/Accessibility-image.png)
+
+# deployment
+
+The live link can be found here -
+
+
+# Credits
+
+Debugging can be divided in three part;
+
+1. Conclusion to onclick function.
+2. Conclusion of localStorage, helped by mentor.
+3. Conclusion of delete all rows,
+
+Checked by Tutor.
+
+
+# content
+
+The Idea has taken from a video: 
+
+
+https://youtu.be/RYE0QQKJI9o
+
+
+# Media
+
+
+Coding idea taken from:
+
+https://stackoverflow.com/questions/13049268/populate-table-using-submited-form-javascript
+
+LocalStorage idea taken from:
+
+https://www.w3schools.com/jsref/met_storage_setitem.asp
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,100 +6,104 @@ const deleteHTML = `<button onclick="onClick(this);">Delete</button>`;
 
 
 function getCurrentLocalStorageValue() {
-    let currentData = localStorage.getItem("data") 
-    try {
-       currentData = JSON.parse(currentData) || [];
-   } catch {
-       currentData = [];
-   } 
-
-   return currentData;
+	let currentData = localStorage.getItem("data") 
+	try {
+		currentData = JSON.parse(currentData) || [];
+	} catch {
+		currentData = [];
+	} 
+	
+	return currentData;
 }
 
 function addDataToLocalStorage(newRow) {
-    const currentData = getCurrentLocalStorageValue();
-    const newData = [...currentData, newRow];
-    localStorage.setItem("data", JSON.stringify(newData)); 
+	const currentData = getCurrentLocalStorageValue();
+	const newData = [...currentData, newRow];
+	localStorage.setItem("data", JSON.stringify(newData)); 
 }
 
 function tableDetails(event) {
-    //event.preventDefault();
-
-    let name = document.getElementById('name').value;
-    let date = document.getElementById('date').value;
-    let type = document.getElementById('type').value;
-    let amount = document.getElementById('amount').value;
-    
-
-    if (!name || !date || !type || !amount) {
-        alert(`Unknown game type`);
-        return;
-    }
- 
-   let tableElement = document.getElementById('expense-table');
-
-   let rows = tableElement.insertRow(-1);
- 
-    let cell1 = rows.insertCell(0);
-    let cell2 = rows.insertCell(1);
-    let cell3 = rows.insertCell(2);
-    let cell4 = rows.insertCell(3);
-    let cell5 = rows.insertCell(4);
-
-    cell1.textContent = name;
-    cell2.textContent = date;
-    cell3.textContent = type;
-    cell4.textContent = amount;
-    cell5.innerHTML = deleteHTML;
-
-    addDataToLocalStorage({name, date, type, amount});
+	//event.preventDefault();
+	
+	let name = document.getElementById('name').value;
+	let date = document.getElementById('date').value;
+	let type = document.getElementById('type').value;
+	let amount = document.getElementById('amount').value;
+	
+	
+	if (!name || !date || !type || !amount) {
+		alert(`Unknown game type`);
+		return;
+	}
+	
+	let tableElement = document.getElementById('expense-table');
+	
+	let rows = tableElement.insertRow(-1);
+	
+	let cell1 = rows.insertCell(0);
+	let cell2 = rows.insertCell(1);
+	let cell3 = rows.insertCell(2);
+	let cell4 = rows.insertCell(3);
+	let cell5 = rows.insertCell(4);
+	
+	cell1.textContent = name;
+	cell2.textContent = date;
+	cell3.textContent = type;
+	cell4.textContent = amount;
+	cell5.innerHTML = deleteHTML;
+	
+	addDataToLocalStorage({name, date, type, amount});
+	resetForm();
 }
 
 function resetForm() {
-    document.getElementById('name').value = "";
-    document.getElementById('date').value = "";
-    document.getElementById('type').value = "";
-    document.getElementById('amount').value = "";
+	document.getElementById('name').value = "";
+	document.getElementById('date').value = "";
+	document.getElementById('type').value = "";
+	document.getElementById('amount').value = "";
 }
 
 function onClick(td) {
-    if (confirm('Are You Sure')) {
-        row = td.parentElement.parentElement;
-        document.getElementById('expense-table').deleteRow(row.rowIndex);
-        resetForm();
-    }
-
-    return;
+	if (confirm('Are You Sure')) {
+		row = td.parentElement.parentElement;
+		document.getElementById('expense-table').deleteRow(row.rowIndex);
+		resetForm();
+	}
+	
+	return;
 }
 
 function tableReset(){
-    if (confirm('Are you sure')) {
-        
-        localStorage.setItem("data", JSON.stringify(""))
-        
-    };
-        
-    }
+	if (confirm('Are you sure to Delete all')) {
+		const tableElement = document.getElementById('expense-table');
+		localStorage.setItem("data", "[]");
+		const tableRows = Array.from(tableElement.children[0].children);
+		const [, ...childRows] = tableRows;
+		
+		childRows.forEach((eachRow) => eachRow.remove());
+	}
+}
+
 
 
 
 function initialiseTable() {
-    const tableData = getCurrentLocalStorageValue();
-    const tableElement = document.getElementById('expense-table');
-    tableData.forEach((eachRow) => {
-        let rows = tableElement.insertRow(-1);
-        let cell1 = rows.insertCell(0);
-        let cell2 = rows.insertCell(1);
-        let cell3 = rows.insertCell(2);
-        let cell4 = rows.insertCell(3);
-        let cell5 = rows.insertCell(4);
-
-        cell1.textContent = eachRow.name;
-        cell2.textContent = eachRow.date;
-        cell3.textContent = eachRow.type;
-        cell4.textContent = eachRow.amount;
-        cell5.innerHTML = deleteHTML;
-    });
+	const tableData = getCurrentLocalStorageValue();
+	const tableElement = document.getElementById('expense-table');
+	tableData.forEach((eachRow) => {
+		let rows = tableElement.insertRow(-1);
+		let cell1 = rows.insertCell(0);
+		let cell2 = rows.insertCell(1);
+		let cell3 = rows.insertCell(2);
+		let cell4 = rows.insertCell(3);
+		let cell5 = rows.insertCell(4);
+		
+		cell1.textContent = eachRow.name;
+		cell2.textContent = eachRow.date;
+		cell3.textContent = eachRow.type;
+		cell4.textContent = eachRow.amount;
+		cell5.innerHTML = deleteHTML;
+	});
 }
 
 document.onload = initialiseTable();
